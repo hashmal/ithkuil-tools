526bbab19466a33cd8870825b1612cb2c473e896
Jérémy Pinat <jeremypinat@bravebolt.com>
2025-02-25 17:38:51 +0000

    Test most consonants

--- spec/ipa/ipa-conversion-rules.spec.ts
+++ spec/ipa/ipa-conversion-rules.spec.ts
@@ -98,3 +98,46 @@ describe('ü', () => {
     expect(ipa('ü')).toEqual('y')
   })
 })
+
+// h is never silent and is pronounced [h] in all positions where it appears, even in word-final position. Syllable-initial or word-final -ph-, -th-, -kh-, -ch-, -čh- are pronounced as aspirated stops/affricates [pʰ, tʰ, kʰ, tsʰ, tʃʰ]but when between two vowels, they are disyllabic and pronounced as in English haphazard, at-hand, backhanded, it’s here and church hall; the combinations -hl-, -hr-, -hm- and -hn- may be pronounced as separate consonants or as the following single voiceless consonants: hl = [ɬ], hr = [ɾ̥], hm = [m̥], hn = [n̥]. Combinations of a voiced consonant plus following h are always dissyllabic, e.g., -bh-, -dh-, -gh-, -rh-, -mh-, -nh-, etc.
+
+// ʼ is the voiceless glottal stop [Ɂ] as heard between the two vowels of English oh-oh!, or between the first two e-sounds of German beendete, or the one possible realization of the zero phoneme in Mandarin 親愛 (qīnʼài [t͡ɕʰji̞n̚˥.ʔa̠ɪ̯˥˩], different from qīnài [t͡ɕʰi˥.na̠ɪ̯˥˩] or qīnnài [t͡ɕʰji̞n̚˥.na̠ɪ̯˥˩]); For the voiceless glottal stop in the syllable initial, cf. the distinctions in Hawaiian or some Ryukyuan languages; For the voiceless glottal stop in the syllable coda, cf. the distinctions in Wu or Min Chinese languages, as well as Japanese あっ (aQ, [äʔ]).
+
+// n is dental, not alveolar; n assimilates to velar [ŋ] before k, g, and x (but not before ř); therefore, phonemic ň is not permitted before k, g, or x in New Ithkuil native words.
+
+// r is a single tap/flap [ɾ], which becomes a trill [r] when geminated, as in Spanish or Italian caro and carro; when followed by a consonant in the same word, it may be pronounced as an apico-alveolar approximant [ɹ], similar to (but further forward in the mouth than) the postalveolar [ɹ̱] of standard English. An example of an apico-alveolar approximant is the non-retroflex r sound in Mandarin.
+
+// ř is the voiced dorso-uvular approximant [ʁ] as in French rire or German Ruhr; when geminated it is either [ʁː] or can be strengthened to a uvular trill [ʀ]; care should be taken to ensure the pronunciations of -př- and -tř- are differentiated from -px- and -tx-.
+
+describe.each([
+  ['p', 'p', 'as in French, Spanish, or Italian'],
+  ['k', 'k', 'as in French, Spanish, or Italian'],
+  ['t', 't̪', 'as in French, Spanish, or Italian'],
+  ['d', 'd̪', 'as in French or Italian'],
+  ['g', 'g', 'as in English "go"'],
+  ['ţ', 'θ', 'as in English "this" or Castilian Spanish "caza"'],
+  ['ḑ', 'ð', 'as in English "thin" or Castilian Spanish "cada"'],
+  ['š', 'ʃ', 'as in English "mesh"'],
+  ['ž', 'ʒ', 'as in English "mesure"'],
+  ['ç', 'ç', 'as in the initial sound of English "human" or "hue"'],
+  ['x', 'x', 'as in either Latin American or Castilian Spanish "jota", Russian "хорошо", German "bach", or Mandarin "h-"'],
+  ['l', 'l̪', 'as in French, Spanish, or Italian'],
+  ['ļ', 'ɬ', 'as found in Welsh "llan"'],
+  ['c', 'ts', 'as in English "bits" or Italian "pizza"'],
+  ['ẓ', 'dz', 'as in English "bids" or Italian "azzurro"'],
+  ['č', 'tʃ', 'as in English "butch"'],
+  ['j', 'dʒ', 'as in English "budge"'],
+  ['ň', 'ŋ', 'as in English "bring"'],
+  ['y', 'j', 'as in English "yes"'],
+])('%s', (ithkuilCharacter: string, ipaCharacter: string, description?: string) => {
+  const title = description ? `is pronounced [${ipaCharacter}] ${description}` : `is pronounced [${ipaCharacter}]`
+  it(title, () => {
+    expect(ipa(ithkuilCharacter)).toEqual(ipaCharacter)
+  })
+})
+
+describe.each(['b', 'f', 'm', 's', 'v', 'w', 'z'])('%s', (letter) => {
+  it(`is pronounced [${letter}] as in English`, () => {
+    expect(ipa(letter)).toEqual(letter)
+  })
+})
