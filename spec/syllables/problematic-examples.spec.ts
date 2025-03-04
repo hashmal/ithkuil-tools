d58fddcc55a8d41f7749ea9c3ae39d366518cad1
Jérémy Pinat <jeremypinat@bravebolt.com>
2025-03-04 17:16:02 +0000

    Add `wezvwauš' to problematic examples

--- spec/syllables/problematic-examples.spec.ts
+++ spec/syllables/problematic-examples.spec.ts
@@ -3,6 +3,7 @@ import { split } from '../helpers'
 
 describe.skip.each([
   ['uẓfäl', 'u-ẓfäl'], // [u.dzfæl̪] instead of [udz.fæl̪]
+  ['wezvwauš', 'we-zvwauš'], // [wɛ.zvwauʃ] instead of [wɛz.vwauʃ]
 ])('%s', (word: string, syllables: string) => {
   it(`resolves to ${syllables}`, () => {
     expect(romanizedIthkuilToSyllables(word)).toEqual(split(syllables))
