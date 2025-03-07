f92c14f952a244becb2e0a0a455c8fc22d28e592
Jérémy Pinat <jeremypinat@bravebolt.com>
2025-03-07 04:22:48 +0000

    Add a problematic example

--- spec/syllables/problematic-examples.spec.ts
+++ spec/syllables/problematic-examples.spec.ts
@@ -5,13 +5,14 @@ import { split } from '../helpers'
  * Expected behavior is explained in the comments.
  * Fully commented specs are considered solved.
  */
-describe.skip.each([
+describe.each([
   ['uẓfäl', 'u-ẓfäl'], // [u.dzfæl̪] instead of [udz.fæl̪]
   // ['wezvwauš', 'wez-vwauš'], // [wɛ.zvwauʃ] instead of [wɛz.vwauʃ]
   // ['wezçauš', 'wez-çauš'], // [wɛ.zçauʃ] instead of [wɛz.çauʃ]
   // ['epssaloʼë', 'e-pssa-lo-ʼë'], // [epss-a-lo-ʼë] instead of [e-pssa-lo-ʼë]
   // ['opsspa', 'opss-pa'], // [o-psspa] instead of [opss-pa]
   ['amfspa', 'amfs-pa'], // [amfs-pa] instead of [amf-spa]
+  ['alpšška', 'alpšš-ka'], // [alpšš-ka] instead of [alp-šška]
 ])('%s', (word: string, syllables: string) => {
   it(`resolves to ${syllables}`, () => {
     expect(romanizedIthkuilToSyllables(word)).toEqual(split(syllables))
